29b5a08ab5fe01fd13329eaa8c723d2dba8f072c
Utkarsh Varun <uv2277@gmail.com>
2025-05-30 02:44:24 +0530

    update 1.1

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
   return (
     <nav className="navbar"> 
       <div className="navbar-brand">
-        <Link to="/">CineScope</Link>
+        <Link to="/"><span className="brand-title">CineScope</span> <span className="slogan">Search and track your favorite movies</span></Link>
       </div>
       <div className="navbar-links">
         <Link to="/" className="nav-link">
